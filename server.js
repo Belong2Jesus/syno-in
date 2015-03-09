deaa17557e4d015028c0bde01b80cc1df4d76b73
root <root@stun2.syno.jp>
2015-03-10 08:33:52 +0900

    http -> https redirect

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
 #!/bin/env node
 var config = require('config');
 var connect = require('connect');
+var http = require('http');
 var https = require('https');
 var fs = require('fs');
 console.log(config);
+
+if (config.isDev === false) {
+  http.createServer(function (req, res) {
+    res.writeHead(301, {'Location': 'https://syno.in' + req.url});
+    res.end();
+  }).listen(80, config.app.ip);
+}
 
 var options = {
     key:    fs.readFileSync(__dirname + '/ssl/startssl.key.nopass'),
     cert:   fs.readFileSync(__dirname + '/ssl/startssl.crt.append'),
     ca:     fs.readFileSync(__dirname + '/ssl/ca.pem')
 };
-// oreore
-//var options = {
-//    key:    fs.readFileSync(__dirname + '/ssl/server_key.pem'),
-//    cert:   fs.readFileSync(__dirname + '/ssl/server_crt.pem')
-//};
-
 var server = https.createServer(options, function(req, res){
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end( fs.readFileSync(__dirname + ( (req.url === "/") ? '/index.html' : '/vchat.html')));
 }).listen(config.app.port, config.app.ip);
-
-// var io = socket.listen(server);
-
 
 var resource = connect().use(connect.static(__dirname + '/pub'));
 https.createServer(options,resource).listen(config.resource.port, config.resource.ip);
