c95f8274d6f39eb69b8e701b9c3d68cb36ed0f7a
vanx2 <happy_yatani@yahoo.co.jp>
2015-08-20 11:59:12 +0900

    fix chat bug

--- server.js
+++ server.js
@@ -5,24 +5,18 @@ var http = require('http');
 var https = require('https');
 var fs = require('fs');
 console.log(config);
-var options={};
 
 if (config.isDev === false) {
   http.createServer(function (req, res) {
-    res.writeHead(301, {'Location': 'https://syno.in' + req.url});
+    res.writeHead(301, {'Location': 'https://'+ config.app.host + req.url});
     res.end();
   }).listen(80, config.app.ip);
-  options = {
-      key:    fs.readFileSync(__dirname + '/ssl/startssl.key.nopass'),
-      cert:   fs.readFileSync(__dirname + '/ssl/startssl.crt.append'),
-      ca:     fs.readFileSync(__dirname + '/ssl/ca.pem')
-  };
-} else {
-  options = {
-      key:    fs.readFileSync(__dirname + '/ssl_dev/key.pem'),
-      cert:   fs.readFileSync(__dirname + '/ssl_dev/cert.pem')
-  };
 }
+  var options = {
+      key:    fs.readFileSync(__dirname + '/' + config.ssl.key),
+      cert:   fs.readFileSync(__dirname + '/' + config.ssl.cert)
+  };
+  if(config.ssl.ca){options.ca = fs.readFileSync(__dirname + '/' + config.ssl.ca);}
 
 var server = https.createServer(options, function(req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
@@ -32,22 +26,6 @@ var server = https.createServer(options, function(req, res) {
 var resource = connect().use(connect.static(__dirname + '/pub'));
 https.createServer(options,resource).listen(config.resource.port, config.resource.ip);
 
-/*
-var server = http.createServer(app);
-var io     = require('socket.io').listen(server);
-
-io.sockets.on('connection', function (socket) {
-    var ip = socket.handshake.headers['x-forwarded-for'] ||
-             socket.handshake.address.address;
-
-    socket.on('join', function (data) {
-      console.log(data);
-    });
-
-//  socket.emit('init', { hello: 'world' });
-
-});
-*/
 
 
 
